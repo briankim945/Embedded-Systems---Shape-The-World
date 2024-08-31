188f09518c80ddc866e5b98168c3c5b18219cbeb
briankim945 <bsk945@gmail.com>
2024-08-31 15:05:05 +0100

    Completed lab 5

---
+++ Lab5_FunctionsInC/main.d
@@ -0,0 +1,4 @@
+.\main.o: main.c
+.\main.o: C:\Users\Brian\AppData\Local\Keil_v5\ARM\ARM Default 5\Bin\..\include\stdio.h
+.\main.o: uart.h
+.\main.o: TExaS.h
---
+++ Lab5_FunctionsInC/uart.d
@@ -0,0 +1,3 @@
+.\uart.o: UART.c
+.\uart.o: UART.h
+.\uart.o: C:\Users\Brian\AppData\Local\Keil_v5\ARM\ARM Default 5\Bin\..\include\stdio.h
